a2be4e5d95d18763758816aaddcc7c8dcb5aa2b3
Chad <chadbaily27@gmail.com>
2018-01-29 20:26:08 -0500

    Update to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ImagesComponent } from './images.component';
 import { IndexComponent } from './index/index.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, ImagesComponent, IndexComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ImagesComponent,
+    IndexComponent,
+    ContactComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [ImagesComponent],
   providers: [],
   bootstrap: [AppComponent]
